8605fb7f1eb17dbcc75f8ac3969f1f3a64ecb6d3
Catalin Ursu <catalin.ursu@softelligence.net>
2022-09-13 19:31:49 +0300

    update curs7

--- src/main/java/Curs7/IsEnabled.java
+++ src/main/java/Curs7/IsEnabled.java
@@ -1,21 +1,39 @@
 package Curs7;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 import utils.BaseTest;
 
-public class IsEnabled extends BaseTest {
+public class IsEnabled extends BaseTest{
 	
 	@Test
-	public void isEnabledExample() {
+	public void isEnabledExample() throws InterruptedException {
 		
-		WebElement loginPopup = driver.findElement(By.cssSelector("li>a[href='#popup_login']"));
+		WebElement loginPopUp = driver.findElement(By.cssSelector("li[class='menu_user_login']"));
+
+		System.out.println(loginPopUp.isEnabled());
+			
+		JavascriptExecutor jse = (JavascriptExecutor)driver;
+		jse.executeScript("arguments[0].setAttribute('disabled','');", loginPopUp);	
+		System.out.println(loginPopUp.isEnabled());
 		
-		System.out.println(loginPopup.isEnabled());
+		loginPopUp.click();
+		
+		WebElement usernameFiled = driver.findElement(By.id("log"));
+		//usernameFiled.click();
+		System.out.println("username : " + usernameFiled.isEnabled());
+		jse.executeScript("arguments[0].setAttribute('disabled','');", usernameFiled);	
+		usernameFiled.sendKeys("TestUser");
+		System.out.println("username2 : " + usernameFiled.isEnabled());
+
+		Thread.sleep(3000);
+		
+		
 		
 	}
+	
 
 }
