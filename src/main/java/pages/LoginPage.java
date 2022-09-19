090c0a8a75dc1b25fccc6f89bb140b7419d9b246
Catalin Ursu <catalin.ursu@softelligence.net>
2022-09-19 09:25:17 +0300

    curs9 loginTest

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,5 +1,14 @@
 package pages;
 
-public class LoginPage {
+import org.openqa.selenium.WebDriver;
 
+public class LoginPage {
+	
+public WebDriver driver;
+	
+	public LoginPage (WebDriver driver) {
+		this.driver = driver;
+
+	}
+	
 }
