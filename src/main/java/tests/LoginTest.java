7ab39f953a93fecb924d1f82fddf155070e4ce98
Catalin Ursu <catalin.ursu@softelligence.net>
2022-09-19 11:00:55 +0300

    curs9

--- src/main/java/tests/LoginTest.java
+++ src/main/java/tests/LoginTest.java
@@ -1,7 +1,10 @@
 package tests;
 
+import static org.testng.Assert.assertTrue;
+
 import org.testng.annotations.Test;
 
+import pages.LoginPage;
 import pages.NavMenuPage;
 import utils.BaseTest;
 
@@ -11,6 +14,11 @@ public class LoginTest extends BaseTest {
 	public void loginTest() {
 		
 		NavMenuPage navMenu = new NavMenuPage(driver);
+		navMenu.navigateTo(navMenu.loginLink);
+		LoginPage logpage = new LoginPage(driver);
+		logpage.loginapp("TestUser", "12345@67890");
+		assertTrue(logpage.loginSuccessMessage());
+		logpage.logout();
 		
 	}
 
