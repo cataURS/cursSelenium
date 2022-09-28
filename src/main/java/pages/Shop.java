b212decec65b517e09ea723f285e88f79ed0c3f4
Catalin Ursu <catalin@academicmerit.com>
2022-09-28 18:47:56 +0300

    curs12

--- src/main/java/pages/Shop.java
+++ src/main/java/pages/Shop.java
@@ -2,11 +2,15 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 public class Shop {
 	
 public WebDriver driver;
-	
+public WebElement dropdown;
+public Select select;
+
 	public Shop(WebDriver driver) {
 		this.driver = driver;
 		
@@ -21,6 +25,33 @@ public WebDriver driver;
 	public By amount = By.xpath("//div[@class='quantity']");
 	public By addCart = By.xpath("//button[@type='submit' and @name='add-to-cart']");
 	public By tags = By.xpath("//div[@class='product_meta']");
+	//curs12
+	public By orderDropdown = By.xpath("//select[@name='orderby']");
+	
+	public void selectByValue(String value) {
+		dropdown = driver.findElement(orderDropdown);
+		select = new Select(dropdown);
+		select.selectByValue(value);
+	}
+	
+	public void selectByIndex(int index) {
+		dropdown = driver.findElement(orderDropdown);
+		Select select = new Select(dropdown);
+		select.selectByIndex(index);
+	}
+	
+	public void selectByVisibleText(String text) {
+		dropdown = driver.findElement(orderDropdown);
+		Select select = new Select(dropdown);
+		select.selectByVisibleText(text);
+	}
+	
+	public String getSelectedOption() {
+		dropdown = driver.findElement(orderDropdown);
+		Select select = new Select(dropdown);
+		return select.getFirstSelectedOption().getText();
+	}
+	
 	
 	public boolean validate(By locator) {
 		   return  driver.findElement(locator).isDisplayed();
